e69b399d3586e046b03a2c84ffdac5e4a14dd55b
gobeltri <gobeltri@gmail.com>
2018-03-21 10:24:36 +0000

    Adding format to table with Bootstrap Table library

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,5 @@
+/*global $*/
+
 // Make an AJAX request and add a table with results on a given section.
 load_api_table('GET', '/api/v1', 'fees-section', ['ticker', 'binance_fee_usd', 'bittrex_fee_usd', 'poloniex_fee_usd']);
 
@@ -7,7 +9,6 @@ load_api_table('GET', '/api/v1', 'fees-section', ['ticker', 'binance_fee_usd', '
    @api_url: URL from API to call
    @section_id: Section ID in target DOM
    @keys: List of dictionary keys to print in table
-
 */
 function load_api_table(method, api_url, section_id, keys) {
     var xhttp = new XMLHttpRequest();
@@ -15,6 +16,9 @@ function load_api_table(method, api_url, section_id, keys) {
         if (this.readyState == 4 && this.status == 200) {
             var html = jsonlist2table(this.responseText, keys);
             document.getElementById(section_id).innerHTML = html;
+            
+            // Adding Bootstrap format table
+            custom_format_table('#fees-section table');
         }
     };
     xhttp.open(method, api_url, true);
@@ -29,23 +33,58 @@ function jsonlist2table (list, keys) {
     var html = '<table>';
     
     // Keys
-    html += '<tr>';
+    html += '<thead><tr>';
     for (var i in keys) {
         html += '<th>' + keys[i] + '</th>';
     }
-    html += '</tr>';
+    html += '</tr></thead>';
 
     // Values
+    html += '<tbody>';
     for (var i in myObj) {
         html += '<tr>';
         for (var j in keys) {
             var key = keys[j];
-            html += '<td>' + myObj[i][key] + '</td>';
+            
+            // Adding '$' to dollar fields
+            if (j != 0 && myObj[i][key] != '-') 
+                html += '<td class="usd">' + myObj[i][key] + '</td>';
+            else
+                html += '<td>' + myObj[i][key] + '</td>';
         }
         html += '</tr>';
     }
+    html += '</tbody>';
     
     html += "</table>";
 
     return html;
+}
+
+/* Custom format table using Bootstrap table */
+function custom_format_table (table_selector) {
+    
+    $("#fees-section" + ' thead th').each(function() {
+        if ( $(this).html() == 'ticker' )
+            $(this).html('Ticker');
+        else if ( $(this).html() == 'binance_fee_usd' )
+            $(this).html('Binance Fee (USD)');
+        else if ( $(this).html() == 'bittrex_fee_usd' )
+            $(this).html('Bittrex Fee (USD)');
+        else if ( $(this).html() == 'poloniex_fee_usd' )
+            $(this).html('Poloniex Fee (USD)');
+    } )
+    
+
+    $.extend($.fn.bootstrapTable.defaults, {
+        locale: 'en-US',
+  	    search: true,
+  	    searchText: '',
+  	    pagination: true,
+  	    pageSize: 100
+    });
+    $.extend($.fn.bootstrapTable.columnDefaults, {
+  	    sortable: true
+    });
+    $(table_selector).bootstrapTable();
 }
